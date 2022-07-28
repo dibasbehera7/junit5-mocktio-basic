fb20892a4a4dd9f2cde50524fc9ac5c4c6ea807d
dibasbehera7 <dibasbehera@gmail.com>
2022-07-28 15:21:12 +0530

    Initial commit

---
+++ src/test/java/com/dibasb/FirstMockitoTest.java
@@ -0,0 +1,14 @@
+package com.dibasb;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstMockitoTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
